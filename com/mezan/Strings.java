e6d6b2a2d3b240c96df898cd1399ae950afa422e
Mejanur Rahman Akash <mrmezan06@gmail.com>
2021-09-30 15:17:32 +0600

    First Lesson

---
+++ com/mezan/Strings.java
@@ -0,0 +1,11 @@
+package com.mezan;
+
+public class Strings {
+    public static void main(String[] args) {
+        String a = "adsgreAWF dsgsd sdgsRED";
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+        System.out.println(a.length());
+        System.out.println(a.indexOf("g"));
+    }
+}
---
+++ com/mezan/Variables_Types.java
@@ -0,0 +1,21 @@
+package com.mezan;
+
+public class Variables_Types {
+    public static void main(String[] args) {
+        int a;
+        char b;
+        double c;
+        boolean d;
+        String myText = "Hello";
+
+        a = 10;
+        b = 'A';
+        c = 19.99;
+        float e = (float) c;
+        d = true;
+
+        System.out.println(myText+"A="+a +"B="+ b +"C="+ c +"D="+ d +"E="+ e);
+
+
+    }
+}
